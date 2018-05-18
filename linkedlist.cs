26aaa1708fbb949534a8a5d8d5f1403a93261c83
Henrietta Odiete <henriettaodiete@rocketmail.com>
2018-05-18 00:47:25 -0400

    Update linkedlist.cs

--- linkedlist.cs
+++ linkedlist.cs
@@ -3,86 +3,130 @@
 using System; 
 using System.Collections.Generic;
 
-public class LinkedList
+public class Node
 {
-    public class Node  
-    {    
-      int data;
-      Node next;
+  public object data;
+  public Node next;
 
-      Node (int data)
+
+  Node (object data)
+    {
+      this.data = data;
+    } 
+}
+
+public class LinkedList  
+{     
+    public Node head;
+
+    public void Print()
+    {
+      Node current = head; 
+
+      while(current!= null)
       {
-        this.data = data;
-      } 
-      Node head;
-      pubic void Append(int data)
+        Console.WriteLine(current.data);
+        current= current.next;
+      }
+    }
+
+    public void Append(object data)
+    {
+      if(head == null)
       {
-        if(head ==null)
-        {
-          head = new Node(data);
-          return;
-        }
-        Node current = head; 
-        while (current.next!- null)
+        head = new Node(data);
+        head.next = null;
+      
+      }
+      else
+      {
+        Node toAdd = new Node(data);
+        toAdd.data= data; 
+
+        Node current = head;
+
+        while (current.next!= null)
         {
           current= current.next; 
         }
 
-        curren.next = new Node(data); 
-
-      }
-    // adding to the start of the list 
-      public void Prepend(int data)
+        current.next = toAdd; 
+     }
+      
+    }
+  // adding to the start of the list 
+    public void Prepend(object data)
+    {
+      if (head == null)
       {
-        if (head == null)
-        {
-          head = new Node (data);
-        }
-
-        Node temp = new Node(data);
-        temp.next = head; 
-        head = temp;
-
+        head.data =data;
+        head.next = null;
+        return;
       }
 
-      public void Delete (int data)
-      {
-        if (head == null)
-        {
-          return;
-        }
+      Node temp = new Node(data);
+      temp.next = head; 
+      head = temp;
 
-        if (head.data == data)
-        {
-          head = head.next;
-          return;
-        }
-
-        Node current = head; 
-        while (current.next! =null)
-        {
-          if (current.next.data == data)
-          {
-            current.next = current.next.next;
-          }
-          current= current.next;
-        }
-      }
-      static int countNodes(Node head)
-      {
-        int count = 0; 
-        Node current = head; 
-
-        while(current.next!= null)
-        {
-          current = current.next;
-          count++;
-        }
-        return count;
-      }
     }
 
-}    
+    public void Delete (object data)
+    {
+      if (head == null)
+      {
+        return;
+      }
+
+      if (head.data == data)
+      {
+        head = head.next;
+        return;
+      }
+
+      Node current = head; 
+      while (current.next != null)
+      {
+        if (current.next.data == data)
+        {
+          current.next = current.next.next;
+        }
+        current= current.next;
+      }
+
+    }
+    public int countNodes()
+    {
+      int count =0; 
+      Node current = head; 
+
+      while(current.next!= null)
+      {
+        current = current.next;
+        count++;
+      }
+      return count;
+    }
+    
+  }
+
+  class Program 
+  {
+    static void Main()
+    {
+      LinkedList myList = new LinkedList();
+      myList.Append(1);
+      myList.Append(17);
+      myList.Append(18);
+
+      myList.Print(); 
+      myList.Prepend(2);
+
+      myList.Print();
+      int count = myList.countNodes();
+      Console.WriteLine("Number of elements in the linked list is  {0}", count);
+
+    }
+  }
    // test method for generalizedGCD 
 
 
